b0189c4dd70af796b8a1407c2184958dd1eaad7f
unknown <rin217@naver.com>
2024-01-19 16:43:47 +0900

    240119 code.json 추가 및 MainPage 작업

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import '../Main.css'
+import data from '../code.json'
 
 function MainPage() {
 
@@ -10,32 +11,32 @@ function MainPage() {
         {
             id:0,
             digit:['','','','','',''],
-            state:['B','B', 'B', 'B', 'B','B']
+            state:['B','B','B','B','B','B']
         },
         {
             id:1,
             digit:['','','','','',''],
-            state:['B','B', 'B', 'B', 'B','B']
+            state:['B','B','B','B','B','B']
         },
         {
             id:2,
             digit:['','','','','',''],
-            state:['B', 'B', 'B', 'B', 'B', 'B']
+            state:['B','B','B','B','B','B']
         },
         {
             id:3,
             digit:['','','','','',''],
-            state:['B','B', 'B', 'B', 'B','B']
+            state:['B','B','B','B','B','B']
         },
         {
             id:4,
             digit:['','','','','',''],
-            state:['B','B', 'B', 'B', 'B','B']
+            state:['B','B','B','B','B','B']
         },
         {
             id:5,
             digit:['','','','','',''],
-            state:['B','B', 'B', 'B', 'B','B']
+            state:['B','B','B','B','B','B']
         }
     ]);
 
@@ -44,6 +45,12 @@ function MainPage() {
     const [answer, setAnswer] = useState("정답");
     const Hangul = require('hangul-js');
 
+    const keyboardBasic = "flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none ";
+    const keyboardWhite = "bg-slate-200 hover:bg-slate-300 active:bg-slate-400";
+    const keyboardYellow = "bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-white";
+    const keyboardGreen = "bg-green-500 hover:bg-green-600 active:bg-green-700 text-white";
+    const keyboardGrey = "bg-slate-400 text-white";
+
     //console.log(words);
 
     //const arr = [];
@@ -51,7 +58,7 @@ function MainPage() {
     useEffect(() => {
         if(digit < 6) {
             addText(line);
-            console.log(digit);
+            //console.log(digit);
         } else if(digit == 6) {
             addText(line);
             checkText(wordArr);
@@ -62,9 +69,35 @@ function MainPage() {
       }, [wordArr, digit]);
 
       useEffect(() => {
-
+        //console.log("compareArr change line : " + line);
+        //console.log("compareArr change : " + compareArr);
         
       }, [compareArr])
+
+    useEffect(() => {
+
+        //console.log(document.getElementsByClassName("container-sm"));
+
+        //console.log(document.querySelector("#root > div > div.container-sm.p-3 > div:nth-child(1)"));
+
+        console.log("" + document.querySelector("#root > div > div.container-sm.p-3 > div:nth-child(1) > button:nth-child(1)").innerHTML);
+
+        const handleKeyDown = (e) => {
+            
+            let key = data[e.key];
+
+            if(key) getText(key);
+
+            if(e.key == 'Enter') submitWord(words[line].digit);
+
+            if(e.key == 'Backspace') deleteText();
+
+        }
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    })
 
     const getText = text => {
 
@@ -114,12 +147,12 @@ function MainPage() {
             }
 
             words[line].state = compareArr;
+            console.log("compareArr : " + compareArr);
 
             setDigit(0);
             setLine(line + 1);
             setWordArr([]);
-
-            console.log("compareArr : " + compareArr);
+            setCompareArr([]);
         }
 
     }
@@ -139,9 +172,9 @@ function MainPage() {
                 <Word word={words[5]}/>
             </div>
             <div className="container-sm p-3">
-                <div className="flex justify-center mb-1"><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅂ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅈ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㄷ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㄱ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅅ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅛ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅕ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅑ</button></div>
-                <div className="flex justify-center mb-1"><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅁ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㄴ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅇ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㄹ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅎ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅗ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅓ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅏ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅣ</button></div>
-                <div className="flex justify-center"><button onClick={(e)=>{submitWord(words[line].digit)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "65.4px", height: "58px"}}>입력</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅋ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅌ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅊ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅍ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅠ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅜ</button><button onClick={(e)=>{getText(e.target.innerText)}} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "40px", height: "58px"}}>ㅡ</button><button onClick={deleteText} className="flex items-center justify-center rounded mx-0.5 text-base font-bold cursor-pointer select-none bg-slate-200 hover:bg-slate-300 active:bg-slate-400" style={{width: "65.4px", height: "58px"}}>삭제</button></div>
+                <div className="flex justify-center mb-1"><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅂ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅈ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㄷ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㄱ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅅ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅛ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅕ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅑ</button></div>
+                <div className="flex justify-center mb-1"><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅁ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㄴ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅇ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㄹ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅎ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅗ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅓ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅏ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅣ</button></div>
+                <div className="flex justify-center"><button onClick={(e)=>{submitWord(words[line].digit)}} className={keyboardBasic + keyboardWhite} style={{width: "65.4px", height: "58px"}}>입력</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅋ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅌ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅊ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅍ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅠ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅜ</button><button onClick={(e)=>{getText(e.target.innerText)}} className={keyboardBasic + keyboardWhite} style={{width: "40px", height: "58px"}}>ㅡ</button><button onClick={deleteText} className={keyboardBasic + keyboardWhite} style={{width: "65.4px", height: "58px"}}>삭제</button></div>
             </div>
         </div>
         </>
@@ -155,6 +188,7 @@ function Word({word}) {
     const colorGrey = "bg-slate-400 text-white border-slate-400 cell-animation";
     const colorGreen = "bg-green-500 text-white border-green-500 cell-animation";
     const colorYellow = "bg-yellow-500 text-white border-yellow-500 cell-animation";
+
     const colorState = [colorWhite, colorWhite, colorWhite, colorWhite, colorWhite, colorWhite];
 
     for(let i = 0; i < word.state.length; i++) {
@@ -182,3 +216,8 @@ function Word({word}) {
 }
 
 export default MainPage
+
+
+//{word.digit.map((digit, i) => (
+//    <div className={colorBasic + colorState[i]}>{digit}</div>
+//    ))}
